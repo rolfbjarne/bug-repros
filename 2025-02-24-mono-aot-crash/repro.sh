9896abb452647ff4a7039752d4a4430fef1b5792
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-02-24 10:23:43 +0100

    Update script.

--- 2025-02-24-mono-aot-crash/repro.sh
+++ 2025-02-24-mono-aot-crash/repro.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 
-/Users/rolf/work/dotnet/macios/main/macios/builds/downloads/dotnet-sdk-9.0.103-servicing.25065.25/packs/Microsoft.NETCore.App.Runtime.AOT.osx-arm64.Cross.maccatalyst-arm64/9.0.1/Sdk/../tools/mono-aot-cross \
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+/usr/local/share/dotnet/packs/Microsoft.NETCore.App.Runtime.AOT.osx-arm64.Cross.maccatalyst-arm64/9.0.0/Sdk/../tools/mono-aot-cross \
 	--path=./linked \
 	--aot=mtriple=arm64-apple-ios15.0-macabi,data-outfile=./monotouchtest.aotdata,static,asmonly,dedup-skip,direct-icalls,full,soft-debug,dwarfdebug,no-direct-calls,outfile=./monotouchtest.dll.s \
 	--debug -O=gsharedvt \
